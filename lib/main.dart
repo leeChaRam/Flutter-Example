967780f410a75b0fa570a41050d23b13d2e1bb04
leeChaRam <lssgt07@naver.com>
2026-06-12 10:04:12 +0900

    이미지 위젯, 박스 위젯, 아이콘 위젯, 글자 위젯 study

--- lib/main.dart
+++ lib/main.dart
@@ -7,142 +7,24 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
-      home: const CounterScreen(),
-    );
-  }
-}
+      home: Center(
+        child: Container(width: 50, height: 50, color: Colors.blue)
 
-class CounterScreen extends StatefulWidget{
-  const CounterScreen({super.key});
+      ) // 자식위젯의 시작을 정 중앙에 넣겠다.
+      // Container(width: 5, height: 5, color: Colors.blue) // 네모박스
 
-  @override
-  State<CounterScreen> createState() => _CounterScreenState();
-}
+      // 이미지 위젯
+      // Image.asset('miu.gif') 
+      // 1.이미지가 프로젝트 폴더 안에 존재해야함 폴더에 이미지를 넣고
+      // 2.이미지를 쓴다고 등록해야함 pubspec.yaml파일 -> 앱만들때 필요한 모든 자료를 정리한 파일 
+      // 중간쯤에 flutter: 가 보임
 
-class _CounterScreenState extends State<CounterScreen>{
-  int _counter = 0;//화면에 보여줄 숫자 변수 
-
-  void _incrementCounter(){
-    setState((){
-      //setState는 데이터가 변했으니 화면을 다시 그리라는 신호
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('나의 첫 플러터 앱'),
-        backgroundColor: Colors.blue,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('현재 숫자:', style: TextStyle(fontSize: 20)),
-            Text(
-              '$_counter',
-              style: const TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      // Icon(Icons.star) 아이콘 위젯 
+      // Text('안녕') // 글자 위젯
     );
   }
 }
