af9713613bcb62ed6a9ab5419f489f4c5ba6e677
leeChaRam <lssgt07@naver.com>
2026-06-16 10:55:44 +0900

    박스 그리기 예제

--- lib/main.dart
+++ lib/main.dart
@@ -12,29 +12,21 @@ class MyApp extends StatelessWidget {
     
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar( title: Text('앱임')), //상단에 들어갈 위젯
-          body: Column(
-            mainAxisAlignment:  MainAxisAlignment.spaceEvenly, // 가로축 가운데 정렬
-            children : [
-              Icon(Icons.star),
-              Icon(Icons.star),
-              Icon(Icons.star),
-            ]
-          ),// Column : 세로로배열 , Row: 가로로배열
-          bottomNavigationBar: BottomAppBar(
-            child: SizedBox(
-              height: 70, 
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(Icons.phone),
-                  Icon(Icons.message),
-                  Icon(Icons.contact_page),
-                ],
-              ),
-            )
-          ), // 하단에 들어갈 위젯
-      )// 상중하로 나눠주는 위젯
+        appBar: AppBar( title: Text('앱임')),
+        body: Align(
+          alignment: Alignment.topCenter,
+          child:Container(
+            width: double.infinity, height: 50,
+            decoration : BoxDecoration(
+              border: Border.all(color: Colors.black),
+              color: Colors.blue,
+            )// 섬세한 박스 데코 
+          ),
+        )
+      )
+      
+       // 하단에 들어갈 위젯
+      );// 상중하로 나눠주는 위젯
       // 내 자식 위젯의 기준점을 중앙으로 설정해주는 Center()
       //Center(child: Container(width: 50, height: 50, color: Colors.blue)) 
       // Container(width: 5, height: 5, color: Colors.blue) // 네모박스
@@ -47,6 +39,6 @@ class MyApp extends StatelessWidget {
 
       // Icon(Icons.star) 아이콘 위젯 
       // Text('안녕') // 글자 위젯
-    ); //커스터 마이징하고 싶을때는 MaterialApp()을 써야함 
+      //커스터 마이징하고 싶을때는 MaterialApp()을 써야함 
   }
 }
